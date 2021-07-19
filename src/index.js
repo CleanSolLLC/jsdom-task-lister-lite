f04721e6e57a40d72209f9ee39d74a3af24b4f80
mamark90 <mamark90@gmail.com>
2021-07-19 15:04:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  noDefault();
 });
+
+
+function noDefault() {
+  document.getElementById("create-task-form").addEventListener("click", function(event) {
+    event.preventDefault();
+  });
+}
+
+document.getElementsByTagName("input")[1].addEventListener("click", handleClick)
+
+function handleClick(e) {
+  const task = (document.getElementById('new-task-description').value) + ' ';
+  listTask(task);
+  document.getElementById('new-task-description').value="";
+}
+
+  function listTask(task) {
+
+   if (task !== " " ) {
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    li.setAttribute('task-name', task);
+    li.appendChild(document.createTextNode(task))
+    ul.appendChild(li); 
+    let button = document.createElement("button");
+    button.name = task;
+    button.innerHTML = "x";
+
+    button.onclick = function() { deleteButton(task) };
+    li.appendChild(button); 
+    }     
+  }
+
+
+if (document.querySelector("button") !== null) {
+
+    let btn = document.querySelector('button')
+    btn.addEventListener("click", deleteTask)
+}
+
+function deleteButton(task) {
+  let taskVal = task;
+  let taskSel = document.querySelector('li', 'taskVal');
+  taskSel.remove();
+}
+
+
+
+
+
+
